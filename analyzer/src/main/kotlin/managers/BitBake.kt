d481866b333740b22448a4e758deef7986388c66
Kalle Soranko <ksoranko@verifa.io>
2019-10-14 08:04:36 +0300

    make compile after merging with current master

--- analyzer/src/main/kotlin/managers/BitBake.kt
+++ analyzer/src/main/kotlin/managers/BitBake.kt
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ * Copyright (C) 2019 Verifa Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
 package com.here.ort.analyzer.managers
 
 import ch.frankel.slf4k.debug
@@ -14,8 +34,16 @@ import com.here.ort.utils.textValueOrEmpty
 
 import java.io.File
 
-class BitBake(config: AnalyzerConfiguration) : PackageManager(config) {
-    companion object : PackageManagerFactory<BitBake>(
+class BitBake(
+    name: String,
+    analysisRoot: File,
+    analyzerConfig: AnalyzerConfiguration,
+    repoConfig: RepositoryConfiguration
+) {
+    
+    
+    ) : PackageManager(config) {
+        companion object : PackageManagerFactory<BitBake>(
             "https://github.com/openembedded/bitbake",
             "n/a",
             listOf("oe-init-build-env")
